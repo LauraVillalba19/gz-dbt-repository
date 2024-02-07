7cfe5864380908c9f7414246232b07f782b76b51
Laura Villalba <lauravillalbahuaman@gmail.com>
2024-02-07 15:18:39 +0000

    update tables

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -13,7 +13,7 @@ renamed as (
         paid_source,
         campaign_key,
         campgn_name AS campaign_name,
-        CAST(ads_cost AS float64),
+        CAST(ads_cost AS float64)  AS ads_cost,
         impression,
         click
 
