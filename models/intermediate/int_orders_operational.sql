75955596a65599965815a104e0c5fd61649cf205
Laura Villalba <lauravillalbahuaman@gmail.com>
2024-02-07 12:40:55 +0000

    cree int_orders_margin y update int_orders_operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,12 +1,15 @@
--- Operational margin = margin + shipping fee - log_cost - ship_cost
-
 SELECT
-  orders_id,
-  max(date_date) as date_date,
-  ROUND(SUM(revenue),2) as revenue,
-  ROUND(SUM(quantity),2) as quantity,
-  ROUND(SUM(purchase_cost),2) as purchase_cost,
-  ROUND(SUM(margin),2) as margin
-FROM {{ ref("int_sales_margin") }}
-GROUP BY orders_id
-ORDER BY orders_id DESC
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc
