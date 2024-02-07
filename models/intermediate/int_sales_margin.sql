1566d1b60d5d9930be6726adaf66ccd77c860791
Laura Villalba <lauravillalbahuaman@gmail.com>
2024-02-07 12:42:56 +0000

    update int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,14 +1,15 @@
-select
+SELECT 
     products_id, 
     date_date, 
     orders_id,
     revenue, 
     quantity, 
+    CAST(purchase_price AS FLOAT64), 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
-from {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p
-using(products_id)
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
 
 
 
